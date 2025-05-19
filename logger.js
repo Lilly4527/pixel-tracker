75e4517f0179a6e0c00d75ff159f6bd1729095d9
Lilly4527 <anvil4649@proton.me>
2025-05-20 01:49:50 +0200

    Update logger.js

--- logger.js
+++ logger.js
@@ -1,20 +1,16 @@
 (async () => {
-  const ipRes = await fetch("https://ipapi.co/json/");
-  const ipData = await ipRes.json();
-
-  const time = new Date().toLocaleTimeString();
-  const message = `🛎️ Новий візит:
-🌍 GEO: ${ipData.country_name} (${ipData.country})
-🔗 URL: ${window.location.href}
-🕒 Час: ${time}
-📱 Device: ${navigator.userAgent}`;
-
   const token = '7717372931:AAE_IOW2aD4iqgSJTFHhcRetN3JVQNZpLg8';
   const chat_id = '7130547584';
 
+  const ipData = await fetch('https://ipapi.co/json/').then(r => r.json());
+  const device = navigator.userAgent;
+  const time = new Date().toLocaleTimeString();
+
+  const message = `⚠️ Новий візит:\n🌐 GEO: ${ipData.country_name} (${ipData.country})\n🔗 URL: ${window.location.pathname}\n🕒 Час: ${time}\n📱 Device: ${device}`;
+
   fetch(`https://api.telegram.org/bot${token}/sendMessage`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify({ chat_id, text: message })
   });
 })();
