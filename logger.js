84b81c5cdbeb0c198065b56ad54cd09f7cd09157
Lilly4527 <anvil4649@proton.me>
2025-05-20 00:45:58 +0200

    Update logger.js

--- logger.js
+++ logger.js
@@ -1,12 +1,22 @@
-fetch("https://api.telegram.org/bot<7717372931:AAE_IOW2aD4iqgSJTFHhcRetN3JVQNZpLg8>/sendMessage", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json"
-  },
-  body: JSON.stringify({
-    chat_id: "<7130547584)>",
-    text: `🔍 Новий візит на сайт\nIP: [не видно в CDN],\nЧас: ${new Date().toLocaleString()}\nUser-Agent: ${navigator.userAgent}`
-  })
-});
 <head> 
 <script src="https://pixel-tracker.vercel.app/logger.js"></script>
+(async () => {
+  const ipRes = await fetch("https://ipapi.co/json/");
+  const ipData = await ipRes.json();
+
+  const time = new Date().toLocaleTimeString();
+  const message = `🛎️ Новий візит:
+🌍 GEO: ${ipData.country_name} (${ipData.country})
+🔗 URL: ${window.location.href}
+🕒 Час: ${time}
+📱 Device: ${navigator.userAgent}`;
+
+  const token = '7717372931:AAE_IOW2aD4iqgSJTFHhcRetN3JVQNZpLg8';
+  const chat_id = '7130547584';
+
+  fetch(`https://api.telegram.org/bot${token}/sendMessage`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ chat_id, text: message })
+  });
+})();
