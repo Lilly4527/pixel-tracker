26ac1b5dcf42503b0c6cba3b8dacb1c2c2689d47
Lilly4527 <anvil4649@proton.me>
2025-05-20 02:09:51 +0200

    Update logger.js

--- logger.js
+++ logger.js
@@ -1,16 +1,27 @@
 (async () => {
-  const token = '7717372931:AAE_IOW2aD4iqgSJTFHhcRetN3JVQNZpLg8';
-  const chat_id = '7130547584';
+  const chat_id = '7130547584'; 
+  const token = '7717372931:AAE_IOW2aD4iqgSJTFHhcRetN3JVQNZpLg8'; 
 
-  const ipData = await fetch('https://ipapi.co/json/').then(r => r.json());
+  const ipData = await fetch('https://ipapi.co/json/').then(res => res.json());
+
   const device = navigator.userAgent;
   const time = new Date().toLocaleTimeString();
+  const url = window.location.href;
 
-  const message = `⚠️ Новий візит:\n🌐 GEO: ${ipData.country_name} (${ipData.country})\n🔗 URL: ${window.location.pathname}\n🕒 Час: ${time}\n📱 Device: ${device}`;
+  const message = `
+  🚨 Новий візит:
+  🌍 GEO: ${ipData.country_name} (${ipData.country})
+  🔗 URL: ${url}
+  ⏰ Час: ${time}
+  📱 Device: ${device}
+  `;
 
   fetch(`https://api.telegram.org/bot${token}/sendMessage`, {
     method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ chat_id, text: message })
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      chat_id,
+      text: message,
+    })
   });
 })();
